7934719d72660c80a5d09b3498200ece315ad55a
TomSB1423 <tsbushell@outlook.com>
2021-07-01 14:16:50 +0100

    Added Factorial and fibanacci

--- Grokking Algorithms/recursion_playground.py
+++ Grokking Algorithms/recursion_playground.py
@@ -1,3 +1,4 @@
+### Print out values from nested array ###
 def print_values(array):
     for i in range(len(array)):
         if type(array[i]) is list:
@@ -12,5 +13,29 @@ my_array = [
     [7, 8, 9],
     [10, 11, 12],
 ]
+# print_values(my_array)
 
-print_values(my_array)
+
+### Factorial ###
+def factorial(x):
+    if x <= 1:
+        return 1
+    # Product of n * (n-1)
+    return x * factorial(x - 1)
+
+
+# print(factorial(5))
+
+
+### Fibonacci Sequence ###
+def fib(x):
+    # First two terms are 1
+    if x <= 2:
+        return 1
+    # If not ended, sum of two previous terms
+    else:
+        return fib(x - 1) + fib(x - 2)
+
+
+# 1, 1, 2, 3, 5, 8, 13, 21, 34
+print(fib(5))
