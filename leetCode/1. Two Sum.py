a68b3dad9b9238515fbc93d9833128c8449fc237
Tom <tsbushell@outlook.com>
2021-09-16 22:12:08 +0100

    Update 1. Two Sum.py

--- leetCode/1. Two Sum.py
+++ leetCode/1. Two Sum.py
@@ -1,3 +1,4 @@
+# Dict method
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -11,3 +12,22 @@ class Solution(object):
                 store[target - nums[i]] = i
             else:
                 return [store[nums[i]], i]
+
+
+# Alt two pointer method
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        x = []
+        nums = enumerate(nums)
+        nums = sorted(nums, key=lambda x: x[1])
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            if nums[l][1] + nums[r][1] == target:
+                x.append(nums[l][0])
+                x.append(nums[r][0])
+                return x
+            elif nums[l][1] + nums[r][1] > target:
+                r -= 1
+            elif nums[l][1] + nums[r][1] < target:
+                l += 1
