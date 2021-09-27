73f5e3ff07595b2d5e2021310a234b3b4862b913
TomSB1423 <tsbushell@outlook.com>
2021-09-27 12:37:02 +0100

    Update 78. Subsets.py

--- leetCode/78. Subsets.py
+++ leetCode/78. Subsets.py
@@ -1,9 +1,26 @@
+# class Solution:
+#     def subsets(self, nums):
+#         ans = [[]]
+#         for val in nums:
+#             ans += [[val] + x for x in ans]
+#         return ans
+
+# Backtracking solution
 class Solution:
     def subsets(self, nums):
-        ans = [[]]
-        for val in nums:
-            ans += [[val] + x for x in ans]
-        return ans
+        res = []
+        sub = []
+
+        def helper(i):
+            if i >= len(nums):
+                res.append(sub[:])
+                return
+            sub.append(nums[i])
+            helper(i+1)
+            sub.pop()
+            helper(i+1)
+        helper(0)
+        return res
 
 
 s = Solution()
