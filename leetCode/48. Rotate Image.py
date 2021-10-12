763b3aaba3568c73f77bb634c5264d7b9f85375b
TomSB1423 <tsbushell@outlook.com>
2021-10-12 11:25:09 +0100

    Update 48. Rotate Image.py

--- leetCode/48. Rotate Image.py
+++ leetCode/48. Rotate Image.py
@@ -1,13 +1,33 @@
+import math
+from pprint import pprint
+
+
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def print_matrix(self):
+        pprint(self.matrix)
+
+    def rotate(self):
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
+        n = len(self.matrix)
         for i in range(math.ceil(n / 2)):
             for j in range(n // 2):
-                temp = matrix[n - 1 - j][i]
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
-                matrix[j][n - 1 - i] = matrix[i][j]
-                matrix[i][j] = temp
+                temp = self.matrix[n - 1 - j][i]
+                self.matrix[n - 1 - j][i] = self.matrix[n - 1 - i][n - j - 1]
+                self.matrix[n - 1 - i][n - j - 1] = self.matrix[j][n - 1 - i]
+                self.matrix[j][n - 1 - i] = self.matrix[i][j]
+                self.matrix[i][j] = temp
+
+
+s = Solution([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+s.rotate()
+s.print_matrix()
+# [[7, 4, 1],
+#  [8, 5, 2],
+#  [9, 6, 3]]
